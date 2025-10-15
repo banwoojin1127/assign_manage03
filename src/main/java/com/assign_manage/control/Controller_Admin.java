6870a9d889a53bab4d8701da094fe1d984de5f0d
하늘 <pp0240@naver.com>
2025-10-15 14:53:42 +0900

    중복 정리

--- src/main/java/com/assign_manage/control/Controller_Admin.java
+++ src/main/java/com/assign_manage/control/Controller_Admin.java
@@ -6,31 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller_Admin
- */
-public class Controller_Admin extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller_Admin() {
-        super();
-    }
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+@Controller
+@RequestMapping(value = "/user_management") //기본 url 경로 접두
+
+public class Controller_Admin 
+{
+	@RequestMapping(value = "/user_management")
+	
+	public void Sign(HttpServletRequest request, 
+			HttpServletResponse response) throws IOException
+	{
+		System.out.println("SignControl:Sign()...run..");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
 }
