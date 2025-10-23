0bf15a24cd0dde7f6562a03ccec28974bb892cca
하늘 <pp0240@naver.com>
2025-10-23 09:15:08 +0900

    VO_Lecture 유저이름 수강인원 추가

--- src/main/java/com/assign_manage/vo/VO_Lecture.java
+++ src/main/java/com/assign_manage/vo/VO_Lecture.java
@@ -15,6 +15,8 @@ public class VO_Lecture
 	private String start_date;   //시작일
 	private String end_date;     //종료일
 	private String delete_date;  //삭제일
+	private String user_name;	 //유저이름 
+	private int    cnt;			 //수강인원
 	
 	//Getter
 	public int getLecture_no()      { return lecture_no; }
@@ -25,7 +27,8 @@ public class VO_Lecture
 	public String getStart_date()   { return start_date; }
 	public String getEnd_date()     { return end_date; }
 	public String getDelete_date()  { return delete_date; }
-	
+	public String getUser_name() 	{ return user_name;	}
+	public int getCnt() 			{ return cnt; }
 	//Setter
 	public void setLecture_no(int lecture_no)        { this.lecture_no = lecture_no; }
 	public void setId(String id)                     { this.id = id; }
@@ -35,4 +38,7 @@ public class VO_Lecture
 	public void setStart_date(String start_date)     { this.start_date = start_date; }
 	public void setEnd_date(String end_date)         { this.end_date = end_date; }
 	public void setDelete_date(String delete_date)   { this.delete_date = delete_date; }
+	public void setUser_name(String user_name) 		 { this.user_name = user_name;}
+	public void setCnt(int cnt) 					 { this.cnt = cnt; }
+	
 }
